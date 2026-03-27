533c0e2e75cc65c2637d768582c27e4978a5b7cc
samvrutha <samvruthajayarajan@gmail.com>
2026-03-27 11:46:41 +0530

    Added PostgreSQL connection and test route

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,9 @@
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl:{
-    rejectUnauthorized:false
-  }
+const { pool } = require('pg');
+require('dotenv').config();
+const pool = new pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-module.exports = pool.promise();
+module.exports = pool;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 
+const pool = require('./db'); 
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database connection error');
+  }
+});
 
 app.use('/api/users', userRoutes);
 
@@ -18,5 +30,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
